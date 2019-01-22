8d208cd63dff190a385b1d8ad581be8989717b86
Unknown <unknown@example.com>
2019-01-22 15:36:54 +0100

    models catalogue

--- catalogue/api_catalogue/index.php
+++ catalogue/api_catalogue/index.php
@@ -1,29 +1,30 @@
 <?php
-use api\Controllers;
-use api\Responses;
-use api\Errors;
-use Slim\App;
-use Slim\Container;
+	use api\Controllers;
+	use api\Responses;
+	use api\Errors;
+	use Slim\App;
+	use Slim\Container;
 
-require_once('../src/vendor/autoload.php');
+	require_once('../src/vendor/autoload.php');
 
-$configuration = ['settings' => [
-	'displayErrorDetails' => true,
-	'production' => false ]
-];
+	$configuration = ['settings' => [
+		'displayErrorDetails' => true,
+		'production' => false ]
+	];
 
-$c = new Container($configuration);
-Errors\JsonErrorsDispatcher::dispatch($c);
-$app = new App($c);
+	$c = new Container($configuration);
+	Errors\JsonErrorsDispatcher::dispatch($c);
+	$app = new App($c);
 
-// JSON API Routes definitions
-$app->group('', function(){ // Only for group logic to add a middleware (https://www.slimframework.com/docs/v3/objects/router.html#route-groups)
-    $this->get('/categories', Controllers\Categories\Categories::class . ':all');
-})->add(Responses\JsonHeaders::class);
+	// JSON API Routes definitions
+	$app->group('', function(){ // Only for group logic to add a middleware (https://www.slimframework.com/docs/v3/objects/router.html#route-groups)
+	    $this->get('/categories', Controllers\Categories\Categories::class . ':all');
+	})->add(Responses\JsonHeaders::class);
 
-// Run app
-try {
-    $app->run();
-} catch (Exception $e) {
-    die($e->getMessage());
-}
+	// Run app
+	try {
+	    $app->run();
+	} catch (Exception $e) {
+	    die($e->getMessage());
+	}
+?>
---
+++ catalogue/src/api/Controllers/Categories/Sand2Cat.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace api\Controllers\Categories;
+
+use api\Responses\JsonResponse;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Created by PhpStorm.
+ * User: benjaminrobinet
+ * Date: 2018-12-13
+ * Time: 08:50
+ */
+
+class Sand2Cat{
+    protected $container;
+
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
+    public function all(RequestInterface $request, ResponseInterface $response){
+        $response = JsonResponse::make($response, []);
+    }
+}
