fcc70df511c586e2ff84959f57cca41a5bcb5d9c
Benjamin Robinet <benjamin.robinet@free.fr>
2019-02-12 17:06:24 +0100

    fix

--- catalogue/src/api/Responses/CollectionResponse.php
+++ catalogue/src/api/Responses/CollectionResponse.php
@@ -5,17 +5,19 @@ use Psr\Http\Message\ResponseInterface;
 
 class CollectionResponse
 {
-    public static function make(ResponseInterface $response, $data, $pagination = null, $code = 200, $local = "fr-FR"){
+    public static function make(ResponseInterface $response, $data, $links = null, $code = 200, $local = "fr-FR"){
 
         $resp = [
             "type" => "collection",
             "count" => array_values($data)[0]->count(),
             "size" => count(array_values($data)[0]),
-            "links" => $pagination,
         ];
 
         $resp = array_merge($resp, $data);
 
+        if($links !== null){
+            $resp['links'] = $links;
+        }
 
         return JsonResponse::make($response, $resp, $code, $local);
     }
