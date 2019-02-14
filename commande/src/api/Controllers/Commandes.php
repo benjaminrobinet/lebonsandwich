34d7f383269581554cbc4dd81e5866ab0b17ff2f
Benjamin Robinet <benjamin.robinet@free.fr>
2019-02-14 11:05:36 +0100

    commandes update

--- commande/src/api/Controllers/Commandes.php
+++ commande/src/api/Controllers/Commandes.php
@@ -22,15 +22,20 @@ class Commandes{
         $this->container = $container;
     }
 
+    public function single(RequestInterface $request, ResponseInterface $response, $args){
+
+    }
+
     public function all(RequestInterface $request, ResponseInterface $response){
 
-        $status = $request->getQueryParam('page', 1);
+        $status = $request->getQueryParam('status', 'any');
         $current_page = $request->getQueryParam('page', 1);
 
     	//Gestion des conditions
         $where = [];
-        $where['status'] = $request->getQueryParam('status', '*');
-
+        if($status !== 'any'){
+            $where['status'] = $status;
+        }
 
 
     	// if($request->getQueryParam("size") > 0){
@@ -40,14 +45,23 @@ class Commandes{
     	// 	$com = $com->get();
     	// }
 
-        $commandes = Commande::where($where)->paginate(10, ['*'], 'page', $current_page);
-        $commandes->withPath($this->container->router->pathFor('commandes'));
+        $commandes = Commande::where($where)->paginate(10, ['id', 'nom', 'created_at', 'livraison', 'status'], 'page', $current_page);
+        $commandes->withPath($this->container->router->pathFor('commandes') . '?status=' . $status);
 
-        var_dump($commandes->toArray());
+        $result = [];
 
-    	die();
+        foreach ($commandes as $commande){
+            $result[] = [
+                'commande' => $commande,
+                'links' => [
+                    'self' => [
+                        'href' => $this->container->router->pathFor('commande', ['id' => $commande->id])
+                    ]
+                ]
+            ];
+        }
 
-        //$response = CollectionResponse::make($response, ['commandes' => $com]);
-        //return $response;
+        $response = CollectionResponse::make($response, ['commandes' => $result], $commandes->total());
+        return $response;
     }
 }
